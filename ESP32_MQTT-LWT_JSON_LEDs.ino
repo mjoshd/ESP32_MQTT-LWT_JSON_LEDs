e9dd4deef878ce1d8190a7239fad30640e43c31c
mjoshd <35782597+mjoshd@users.noreply.github.com>
2018-12-19 12:09:22 -0500

    added web ota and fixed line spacing

--- ESP32_MQTT-LWT_JSON_LEDs.ino
+++ ESP32_MQTT-LWT_JSON_LEDs.ino
@@ -249,7 +249,11 @@ led_effect:
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-#include "FastLED.h"
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <Update.h>
+
+#include <FastLED.h>
 
 /************ WIFI and MQTT Information (CHANGE THESE FOR YOUR SETUP) ******************/
 #define WIFI_SSID           "wifi ssid"                  //<----- change me
@@ -265,6 +269,12 @@ led_effect:
 #define HOSTNAME            "led-2811-back_porch"        //<----- change me
 #define OTA_PASSWORD        "mqtt ota password"          //<----- change me
 #define OTA_PORT            8266
+
+
+
+/**************************** FOR Web OTA **************************************************/
+String WEB_USERNAME   =     "web ota username";          //<----- change me
+String WEB_PASSWORD   =     "web ota password";          //<----- change me
 
 
 
@@ -475,6 +485,95 @@ PubSubClient client(espClient);
 
 
 
+/********************************** For Web OTA *****************************************/
+WebServer server(80);
+
+/*
+ * Login page
+ */
+String loginIndex = 
+"<form name='loginForm'>"
+  "<table width='20%' bgcolor='A09F9F' align='center'>"
+    "<tr>"
+      "<td colspan=2>"
+        "<center><font size=4><b>" + String(HOSTNAME) + " OTA Login</b></font></center>"
+        "<br>"
+      "</td>"
+      "<br>"
+      "<br>"
+    "</tr>"
+    "<tr>"
+      "<td>Username:</td>"
+      "<td><input type='text' size=25 name='userid'><br></td>"
+    "</tr>"
+    "<br>"
+    "<br>"
+    "<tr>"
+      "<td>Password:</td>"
+      "<td><input type='Password' size=25 name='pwd'><br></td>"
+      "<br>"
+      "<br>"
+    "</tr>"
+    "<tr>"
+      "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+    "</tr>"
+  "</table>"
+"</form>"
+"<script>"
+  "function check(form)"
+  "{"
+//    "if(form.userid.value=='admin' && form.pwd.value=='admin'){" // debug
+    "if(form.userid.value=='" + WEB_USERNAME + "' && form.pwd.value=='" + WEB_PASSWORD + "'){"
+      "window.open('/serverIndex')"
+    "}else{"
+      " alert('Error Password or Username')/*displays error message*/"
+    "}"
+  "}"
+"</script>";
+
+
+/*
+ * Server Index Page
+ */
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+  "<input type='file' name='update'>"
+  "<input type='submit' value='Update'>"
+"</form>"
+"<div id='prg'>Progress: 0%</div>"
+"<script>"
+  "$('form').submit(function(e){"
+    "e.preventDefault();"
+    "var form = $('#upload_form')[0];"
+    "var data = new FormData(form);"
+    " $.ajax({"
+      "url: '/update',"
+      "type: 'POST',"
+      "data: data,"
+      "contentType: false,"
+      "processData:false,"
+      "xhr: function() {"
+        "var xhr = new window.XMLHttpRequest();"
+        "xhr.upload.addEventListener('progress', function(evt) {"
+          "if (evt.lengthComputable) {"
+            "var per = evt.loaded / evt.total;"
+            "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+          "}"
+        "}, false);"
+        "return xhr;"
+      "},"
+      "success:function(d, s) {"
+        "console.log('success!')" 
+      "},"
+      "error: function (a, b, c) {"
+      "}"
+    "});"
+  "});"
+"</script>";
+
+
+
 /********************************** START SETUP*****************************************/
 /*** begin standard setup -*/
 void setup() {
@@ -530,9 +629,57 @@ void setup() {
   Serial.println("");
   Serial.println("---------- setup() complete ----------");
   Serial.println("");
-  reconnect();
 
-/*- end standard setup ***/
+
+
+/********************************** For Web OTA *****************************************/
+/*use mdns for host name resolution*/
+  if (!MDNS.begin(HOSTNAME)) { /*  http://HOSTNAME.local  */
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+/********************************** For Web OTA *****************************************/
+
+  reconnect();
+  
 }
 
 
@@ -849,6 +996,8 @@ void loop() {
   client.loop();
 
   ArduinoOTA.handle();
+
+  server.handleClient();
 
 
   //EFFECT BPM
@@ -1235,28 +1384,32 @@ if(effectString == "Xmas"){
     addGlitterColor(90, 255, 0, 0);
     addGlitterColor(85, 255, 0, 0);
     addGlitterColor(80, 255, 0, 0);
+
     addGlitterColor(94, 0, 255, 0);   //green
     addGlitterColor(89, 0, 255, 0);
     addGlitterColor(84, 0, 255, 0);
     addGlitterColor(79, 0, 255, 0);
+
 //    addGlitterColor(95, 255, 0, 0);   //add more red
 //    addGlitterColor(95, 255, 0, 0);
 //    addGlitterColor(90, 255, 0, 0);
 //    addGlitterColor(90, 255, 0, 0);
 //    addGlitterColor(85, 255, 0, 0);
-//    addGlitterColor(85, 255, 0, 0);
-//    addGlitterColor(80, 255, 0, 0);
-//    addGlitterColor(80, 255, 0, 0);
-//    addGlitterColor(94, 0, 255, 0);   //add more green
-//    addGlitterColor(94, 0, 255, 0);
-//    addGlitterColor(89, 0, 255, 0);
-//    addGlitterColor(89, 0, 255, 0);
-//    addGlitterColor(84, 0, 255, 0);
-//    addGlitterColor(84, 0, 255, 0);
-//    addGlitterColor(79, 0, 255, 0);
-//    addGlitterColor(79, 0, 255, 0);
-    addGlitterColor(39, 0, 0, 255);   //blue (comment out if you hate purple)
-//    addGlitterColor(38, 255, 210, 0); //gold (already created when red & green mix)
+    addGlitterColor(85, 255, 0, 0);
+    addGlitterColor(80, 255, 0, 0);
+    addGlitterColor(80, 255, 0, 0);
+
+//    addGlitterColor(95, 0, 255, 0);   //add more green
+//    addGlitterColor(95, 0, 255, 0);
+//    addGlitterColor(90, 0, 255, 0);
+//    addGlitterColor(90, 0, 255, 0);
+//    addGlitterColor(85, 0, 255, 0);
+    addGlitterColor(85, 0, 255, 0);
+    addGlitterColor(80, 0, 255, 0);
+    addGlitterColor(80, 0, 255, 0);
+
+    addGlitterColor(60, 0, 0, 255);   //blue (comment out if you hate purple)
+    addGlitterColor(30, 255, 210, 0); //gold (already created when red & green mix)
     if (transitionTime == 0 or transitionTime == NULL) {
       transitionTime = 20;
     }
